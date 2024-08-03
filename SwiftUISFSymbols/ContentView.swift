2cbd8cef21986fe4796688726a03807a6c4d6e72
mculha <melih.culha.09@gmail.com>
2024-08-03 23:20:50 +0300

    Initial Commit

---
+++ SwiftUISFSymbols/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUISFSymbols
+//
+//  Created by Melih Çulha on 3.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUISFSymbols/SwiftUISFSymbolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISFSymbolsApp.swift
+//  SwiftUISFSymbols
+//
+//  Created by Melih Çulha on 3.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISFSymbolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
