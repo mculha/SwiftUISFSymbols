4aec7073980ad3980d40e744a01d4dd55a414d52
mculha <melih.culha.09@gmail.com>
2024-08-09 23:20:17 +0300

    Adding Breathe Animation View and Its View Model

---
+++ SwiftUISFSymbols/View/Breathe/BreatheAnimationView.swift
@@ -0,0 +1,21 @@
+//
+//  BreatheAnimationView.swift
+//  SwiftUISFSymbols
+//
+//  Created by Melih Çulha on 9.08.2024.
+//
+
+import SwiftUI
+
+struct BreatheAnimationView: View {
+    
+    @State private var viewModel: BreatheAnimationViewModel = .init()
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    BreatheAnimationView()
+}
