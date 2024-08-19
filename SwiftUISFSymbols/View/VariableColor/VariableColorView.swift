27a231cd5c4e88211304cd20eb10ada52e20d9e1
mculha <melih.culha.09@gmail.com>
2024-08-19 23:42:02 +0300

    Adding SF Symbol Variable Color Animation

--- SwiftUISFSymbols/View/VariableColor/VariableColorView.swift
+++ SwiftUISFSymbols/View/VariableColor/VariableColorView.swift
@@ -12,7 +12,29 @@ struct VariableColorView: View {
     @State private var viewModel: VariableColorViewModel = VariableColorViewModel()
     
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 25) {
+            Image(systemName: "square.stack.3d.up")
+                .symbolEffect(.variableColor.reversing.cumulative, options: .repeat(.continuous).speed(3), value: viewModel.isAnimating)
+                .font(.system(size: 60))
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(Color(.rose), Color(.rose).opacity(0.7))
+            
+            Button("Toggle the animation") {
+                withAnimation {
+                    viewModel.isAnimating.toggle()
+                }
+            }
+            .foregroundStyle(.white)
+            .font(.system(size: 16, weight: .semibold))
+            .padding(.vertical, 10)
+            .padding(.horizontal, 20)
+            .background(Color.green)
+            .clipShape(.rect(cornerRadius: 8))
+            
+            Spacer()
+            
+        }
+        .padding(.top, 50)
     }
 }
 
