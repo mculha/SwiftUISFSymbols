5d5e6da1246969ba0ec0a37e219e239a28f87726
mculha <melih.culha.09@gmail.com>
2024-08-27 23:06:59 +0300

    Configuration of Scale View

--- SwiftUISFSymbols/View/Scale/ScaleView.swift
+++ SwiftUISFSymbols/View/Scale/ScaleView.swift
@@ -11,7 +11,29 @@ struct ScaleView: View {
     @State private var viewModel: ScaleViewModel = .init()
     
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 45) {
+            Image(systemName: "homepod.and.homepod.mini.badge.plus.fill")
+                .symbolEffect(.scale.up, isActive: viewModel.isAnimating)
+                .font(.system(size: 100))
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(Color(.rose), Color(.rose).opacity(0.7))
+            
+            Button("Toggle the animation") {
+                withAnimation {
+                    viewModel.isAnimating.toggle()
+                }
+            }
+            .foregroundStyle(.white)
+            .font(.system(size: 16, weight: .semibold))
+            .padding(.vertical, 10)
+            .padding(.horizontal, 20)
+            .background(Color.green)
+            .clipShape(.rect(cornerRadius: 8))
+            
+            Spacer()
+            
+        }
+        .padding(.top, 50)
     }
 }
 
